b70f637437eb757cc6b7be7ed47067368f09c11f
Daniel Thoo <daniel.thoo21@gmail.com>
2025-02-17 21:01:54 -0500

    Add line information

--- main.c
+++ main.c
@@ -8,10 +8,10 @@ int main(int argc, char *argv[]) {
 
   // Create constant. Add OP_CONSTANT bytecode and then the value to the chunk.
   int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT);
-  writeChunk(&chunk, constant);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
 
-  writeChunk(&chunk, OP_RETURN);
+  writeChunk(&chunk, OP_RETURN, 123);
 
   disassembleChunk(&chunk, "test chunk");
   freeChunk(&chunk);
