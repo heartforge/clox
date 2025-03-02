62f0ce5368666e4eae392588dee739e9cdeb083c
Daniel Thoo <daniel.thoo21@gmail.com>
2025-03-02 16:51:40 -0500

    Add stack based VM

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, char *argv[]) {
+  initVM();
+
   Chunk chunk;
   initChunk(&chunk);
 
@@ -19,6 +22,8 @@ int main(int argc, char *argv[]) {
   writeChunk(&chunk, OP_RETURN, 123);
 
   disassembleChunk(&chunk, "test chunk");
+  interpret(&chunk);
+  freeVM();
   freeChunk(&chunk);
   return 0;
 }
